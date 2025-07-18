c658527863dcce544e075e495aa0bed21ceefe28
bhalacsy <balinthalacsy@gmail.com>
2025-07-18 23:20:19 +0200

    Rename hub

--- backend/hubs/ChatHub.cs
+++ backend/hubs/ChatHub.cs
@@ -5,11 +5,11 @@ namespace backend.hubs;
 
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string username, string message) //NOT called in backend
+    public async Task BroadcastMessage(string username, string message) //NOT called in backend
     {
         if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(message)) return;
         
         var payload = new { username, message, timestamp = DateTime.UtcNow };
-        await Clients.All.SendAsync("recvMessage", payload);
+        await Clients.All.SendAsync("RecvMessage", payload);
     }
 }
